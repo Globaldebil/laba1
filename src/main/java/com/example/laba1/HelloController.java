c94a24e404755be27c522932a9094304af57adfe
Юрий <doribidontovyura@gmail.com>
2023-03-21 15:14:12 +0300

    Начало работы, сделал скелет

---
+++ src/main/java/com/example/laba1/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.laba1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class HelloController {
+    StringBuilder main = new StringBuilder();
+    @FXML
+    private Label mainText;
+    private Button eight;
+    @FXML
+    protected void onHelloButtonClick() {
+        main.append(8);
+        mainText.setText(main.toString());
+    }
+}
