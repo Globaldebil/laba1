c2d2e3b15169c383b1584b9c5ea51f1902a82b76
Юрий <doribidontovyura@gmail.com>
2023-03-28 14:07:19 +0300

    Сделал файлы для истории и памяти

--- src/main/java/com/example/laba1/Memory.java
+++ src/main/java/com/example/laba1/Memory.java
@@ -1,5 +1,25 @@
 package com.example.laba1;
 
-public class Memory {
+import java.io.*;
+import java.util.ArrayList;
 
+public class Memory {
+    private static final File file = new File("memory.txt");
+    public static void toMemory(String str, String result) throws IOException {
+        FileWriter to_mem = new FileWriter(file);
+        to_mem.write(str+"\\r\\n");
+        to_mem.write(result+"\\r\\n");
+        to_mem.close();
+    }
+    public static ArrayList<String> readMemory() throws IOException {
+        ArrayList<String> output = new ArrayList<String>();
+        FileReader fileReader = new FileReader(file);
+
+        fileReader.close();
+        return output;
+    }
+
+    public static boolean isEmpty() throws FileNotFoundException {
+        return file.length() == 0;
+    }
 }
