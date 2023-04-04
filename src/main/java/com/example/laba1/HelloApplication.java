3aab29223186fa44595f1a920d0c8ece254e1a1a
Юрий <doribidontovyura@gmail.com>
2023-04-04 17:00:32 +0300

    Добавлены картинки

--- src/main/java/com/example/laba1/HelloApplication.java
+++ src/main/java/com/example/laba1/HelloApplication.java
@@ -4,8 +4,6 @@ import javafx.application.Application;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.shape.SVGPath;
 import javafx.stage.Stage;
 
 import java.io.IOException;
